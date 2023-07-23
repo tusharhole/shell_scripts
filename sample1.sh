0bbbdd89c1ebef4e672bd87832f49a010de90396
tushar <holetushar@gmail.com>
2023-07-23 07:27:22 +0000

    second commited

---
+++ sample1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Hi my name is Tushar"
+echo "Enter your father Name: "
+read father_name 
+echo "My Father Name is $father_name"
---
+++ sample2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "enter the first no"
+read a 
+echo "enter the second no"
+read b 
+
+c=$(($a+$b))
+
+echo "Sum of the $a + $b = $c"
